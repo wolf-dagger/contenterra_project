1c28ce39d5c5664748f85b9446edaab309e88765
wolf <demonwolfisback@gmail.com>
2023-10-29 14:12:43 +0530

    responsive for all devices done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Card from "./components/Card";
 import MainBox from "./components/MainBox";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
       <div className="main p-2">
         <h1 className="text-center text-[25px]">CONTENTERA ASSIGNMENT</h1>
 
-        <div className="main-box p-2 border mt-8">
+        <div className="main-box">
           <MainBox />
         </div>
       </div>
--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -1,11 +1,39 @@
-
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import Card from "./Card";
 
 const MainBox = () => {
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  const fetchData = () => {
+    axios
+      .get("https://www.reddit.com/r/reactjs.json")
+      .then((res) => {
+        const rawData = res.data.data.children;
+        console.log(rawData);
+        setData(rawData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
-      <h2>Main card</h2>
+      <div className="sm:grid sm:grid-cols-1 mb-5 lg:grid lg:grid-cols-2">
+        {data.map((data, index) => (
+          <>
+            <Card key={index} data={data} />
+          </>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MainBox
+export default MainBox;
